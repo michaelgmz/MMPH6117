182e3f87ca826db439033d3da25b8a8e7f454e42
MichaelGMZ <michael_1229@outlook.com>
2021-01-18 18:29:36 +0800

    Update files

---
+++ MMPH6117 Practical.R
@@ -0,0 +1,11 @@
+# Problem 1.1 ----
+for (i in c(1:10)) {
+    print (-3 * i + 7)
+}
+
+# Problem 1.2 ----
+coor_x <- c(1:10)
+gn <- 2 * (-0.5) ^ (coor_x - 1)
+plot(coor_x, gn, type = 'l')
+
+# Problem 2.1 ----
