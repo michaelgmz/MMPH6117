ed5d7ba1721c1a687a89217ad3713aeb8b10208f
GMZ <michael_1229@outlook.com>
2021-03-20 20:52:00 +0800

    Update course materials

---
+++ Tutorial 2.R
@@ -0,0 +1,184 @@
+# Tutorial 2
+flu <- read.csv("fluvaccine.csv")
+
+# Q1 descriptive analysis
+summary(flu)
+
+# cross-tabulation between vaccination and flu infection
+table(flu$vac, flu$flu)
+prop.table(table(flu$vac, flu$flu),1)
+
+with(flu,boxplot(shealth~vac))
+with(flu,boxplot(shealth~flu))
+
+# Q2 fitting a regression to estimate crude vaccination effect
+lr.fv <- glm(flu~vac, data=flu, family=binomial)
+
+summary(lr.fv)
+
+exp(coef(lr.fv)["vac"])
+exp(confint(lr.fv)) # based on likelihood method
+exp(confint.default(lr.fv)) # based on normality
+
+# The crude vaccine effect is 32.6% (95% CI: 0-43.3%) protection
+
+# Q3 confirming confounding for self-reported health
+# standard rules
+lr.vs <- glm(vac~shealth, data=flu, family=binomial)
+lr.fs <- glm(flu~shealth, data=flu, family=binomial)
+
+summary(lr.vs)
+summary(lr.fs)
+
+# change in estimate
+lr.fv <- glm(flu~vac, data=flu, family=binomial)
+lr.fvs <- glm(flu~vac+shealth, data=flu, family=binomial)
+
+summary(lr.fv)
+summary(lr.fvs)
+
+(coef(lr.fvs)[2]-coef(lr.fv)[2])/coef(lr.fv)[2]
+
+# automatic variable selection
+require(MASS)
+stepAIC(lr.fvs)
+
+# all 3 methods will select shealth as a confounder
+
+# Q4 Stratification by shealth
+flu$shealth.cat <- cut(flu$shealth, c(0,5,7,10), label=1:3, right=T, include.lowest=T)
+
+lr.fv.strat <- list()
+for (i in 1:3) {
+lr.fv.strat[[i]] <- glm(flu~vac, data=flu, family=binomial, subset=(shealth.cat==i))
+print(summary(lr.fv.strat[[i]]))
+}
+
+# approximate pooled estimate
+(coef(lr.fv.strat[[1]])[2]+coef(lr.fv.strat[[2]])[2]+coef(lr.fv.strat[[3]])[2])/3
+
+# Q5 Assessing potential confouder - suppose age is a suspected confounder
+# standard rules
+lr.va <- glm(vac~age, data=flu, family=binomial)
+lr.fa <- glm(flu~age, data=flu, family=binomial)
+
+summary(lr.va)
+summary(lr.fa)
+
+# there was no significant association between age and vaccination
+
+# change in estimate
+lr.fvsa <- glm(flu~vac+shealth+age, data=flu, family=binomial)
+
+summary(lr.fvs)
+summary(lr.fvsa)
+
+(coef(lr.fvsa)[2]-coef(lr.fvs)[2])/coef(lr.fvs)[2]
+
+# there was < 10% change in the vaccine effect estimate
+
+# automatic variable selection
+require(MASS)
+stepAIC(lr.fvsa)
+
+# Based on AIC, age will be selected (age is also significant)
+# but no obvious association between age and vaccination
+# similar results for BMI, smoking and sex
+# antibody titers lie in the causal pathway from vaccination to infection
+
+# Q6 effect modification by smoking and age
+lr.fvs.s <- glm(flu~vac+shealth+vac*smoking, data=flu, family=binomial)
+summary(lr.fvs.s)
+
+lr.fvs.a <- glm(flu~vac+shealth+vac*age, data=flu, family=binomial)
+summary(lr.fvs.a)
+
+# there is evidence of the interaction effect between vaccination and smoking
+# indicating the effect modification of smoking
+# there is no significant age-vaccine interaction
+
+# Q7 vaccination effect for smoker and non-smoker.
+# non-smoker
+exp(lr.fvs.s$coef["vac"])
+exp(confint.default(lr.fvs.s)["vac",])
+
+# AOR = 0.36, 95% CI = 0.23-0.58
+
+# smoker
+exp(lr.fvs.s$coef["vac"]+lr.fvs.s$coef["vac:smoking"])
+se.vac.smoker <- sqrt(vcov(lr.fvs.s)[2,2]+vcov(lr.fvs.s)[5,5]+2*vcov(lr.fvs.s)[2,5])
+# or alternatively, using matrix
+# se.vac.smoker <- sqrt(t(c(1,1)) %*% vcov(lr.fvs.s)[c(2,5), c(2,5)] %*% c(1,1))
+exp(lr.fvs.s$coef["vac"]+lr.fvs.s$coef["vac:smoking"] + c(-1,1)*qnorm(0.975)*se.vac.smoker)
+
+# AOR = 0.96, 95% CI = 0.42-2.22
+
+# Q8 assess collinearity 
+require(car)
+vif(lr.fvs.a)
+
+# vif for vac and vac:age is large
+flu$age.ct <- scale(flu$age, scale=F)
+
+lr.fvs.act <- glm(flu~vac+shealth+vac*age.ct, data=flu, family=binomial)
+summary(lr.fvs.act)
+
+# cannot reduce collinearity with binary variable by centering
+
+vif(lr.fvs.s)
+# no collinearity problem for model with vaccine x smoking interaction
+
+# Q9 antibody titer as mediator
+flu$logabT <- log(flu$abT, base=2)
+
+# antibody titer associated with vaccination?
+lr.abTv <- glm(logabT~vac, data=flu)
+summary(lr.abTv)
+
+# antibody titer associated with flu infection?
+lr.fabT <- glm(flu~logabT, data=flu, family=binomial)
+summary(lr.fabT)
+
+# vaccine associated with flu infection?
+summary(lr.fvs.s)
+
+# vaccine effect attenuated?
+lr.fvsabT.s <- glm(flu~vac+shealth+logabT+vac*smoking, data=flu, family=binomial)
+summary(lr.fvsabT.s)
+
+# The data suggests that antibody titer is a mediator
+
+# Q10 stepAIC - stepwise selection
+# start from the basic model, to 
+step.model <- stepAIC(lr.fv,
+    scope = list(upper = ~vac+shealth+bmi+vac*age+vac*smoking, lower = ~vac))
+
+# Q11 residual plot
+final.model <- lr.fvs.s
+
+resid <- rstudent(final.model)
+plot(resid)
+
+# Residual plots for logistic regression, along with binary variables are not straightforward
+# to interpret
+
+# Q12 ROC curve [for reference only] 
+require(ROCR)
+fm.pred <- predict(final.model, type="response")
+
+# ROC curve
+pred <- prediction(fm.pred, flu$flu)
+perf <- performance(pred,"tpr","fpr")
+plot(perf)
+
+# AUROC
+performance(pred,"auc")@y.values[[1]]
+
+# Q13 Summarize results
+exp(final.model$coef)
+exp(confint.default(final.model))
+
+round(exp(cbind(final.model$coef,confint.default(final.model))),2)
+
+
+
---
+++ Tutorial 3.R
@@ -0,0 +1,101 @@
+fish <- read.csv('fishconsumption.csv')
+
+# mark stratified analysis
+fish$strat <- 0
+fish$strat[c(21, 22, 24, 25)] <- 1
+
+fish$logrr <- log(fish$rr)
+fish$se.logrr <- (log(fish$rr.ub)-log(fish$rr.lb))/(2*1.96)
+
+library(metafor)
+
+# fit a fixed effects model
+fish.fe <- rma(yi=logrr, sei=se.logrr, slab=study, method="FE", 
+data=fish, subset=strat==0)
+
+fish.fe
+
+# Overall estimate by fixed effects model
+with(fish.fe, exp(c(b, ci.lb, ci.ub)))
+
+# calculate the overall effect by inverse variance weighting
+ivw.est <- with(fish[fish$strat==0,], sum(logrr/se.logrr^2)/sum(1/se.logrr^2))
+ivw.var <- with(fish[fish$strat==0,], 1/sum(1/se.logrr^2))
+
+# Q-statistics and I2
+summary(fish.fe)
+
+Q <- fish.fe$QE
+I2 <- (Q-(fish.fe$k-1))/Q * 100
+
+I2
+
+# fit a random effects model
+fish.re <- rma(yi=logrr, sei=se.logrr, slab=study, method="REML", 
+data=fish, subset=strat==0)
+
+# Overall estimate by random effects model
+with(fish.re, exp(c(b, ci.lb, ci.ub)))
+
+# forest plot based on the random effects model
+forest(fish.re, transf=exp, refline=1)
+
+# x axis in log scale, with weights
+forest(fish.re, atransf=exp, refline=0, at=log(c(0.1,0.25,1,4,20)), showweights=T)
+
+fish.re <- rma(yi=logrr, sei=se.logrr, slab=paste(study, year, country, sex, sep=", "), method="REML", 
+data=fish, subset=strat==0)
+
+forest(fish.re, atransf=exp, refline=0, at=log(c(0.1,0.25,1,4,20)), 
+xlim=c(-6,5.5), showweights=T, xlab="Relative risk (log scale)")
+text(-6, 28, "Study", pos=4, font=2)
+text(5.5, 28, "Relative risk [95% CI]", pos=2, font=2)
+
+# Funnel plot
+funnel(fish.re, atransf=exp)
+
+# Egger's test
+regtest(fish.re)
+
+# Meta regression on sex
+fish$sex.spec <- 1*(fish$sex!="Both")
+fish.sex <- fish[fish$sex.spec==1,]
+fish.sex$sex <- droplevels(fish.sex$sex)
+
+fish.re.sex <- rma(yi=logrr~sex, sei=se.logrr, slab=study, method="REML", 
+data=fish.sex)
+
+fish.re.sex
+
+fish.re.year <- rma(yi=logrr~year, sei=se.logrr, slab=study, method="REML", 
+data=fish, subset=strat==0)
+
+fish.re.year 
+
+# Sensitivity analysis
+fish$lq <- 0
+fish$lq[fish$study=="Smith"|fish$study=="Colangelo"] <- 1
+
+fish.re.sen <- rma(yi=logrr, sei=se.logrr, slab=study, method="REML", 
+data=fish, subset=(strat==0 & lq==0))
+
+with(fish.re, exp(c(b, ci.lb, ci.ub)))
+with(fish.re.sen, exp(c(b, ci.lb, ci.ub)))
+
+# Leave one out sensitivity analysis
+leave1out(fish.re, transf=exp)
+
+# Trim and fill sensitivity analysis
+fish.cut <- fish[fish$study!="Albanese",]
+
+fish.cut.re <- rma(yi=logrr, sei=se.logrr, slab=paste(study, year, country, sex, sep=", "), method="REML", 
+data=fish.cut, subset=strat==0)
+
+funnel(fish.cut.re, atransf=exp)
+regtest(fish.cut.re)
+
+fish.cut.tf <- trimfill(fish.cut.re)
+with(fish.cut.tf, exp(c(b, ci.lb, ci.ub)))
+
+funnel(fish.cut.tf)
+
